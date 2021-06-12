f934d44486942d32068ea3ddf8c7242440573680
Alvaro R. S. Dziadzio <dziadzio.alvaro@gmail.com>
2021-06-12 15:00:48 -0300

    🚧 Add svelte-navigator

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,37 @@
 <script>
+	import { Router, Link, Route } from "svelte-navigator";
 	import Tailwind from "./Tailwind.svelte";
 </script>
 
 <Tailwind/>
 
-<main class="p-8">
-	<h1 class="text-7xl text-center text-red-700">Hello Svelte!</h1>
-	<h2 class="text-xl text-center mt-4">with tailwind</h2>
-</main>
+<Router>
+	<div class="p-8">
+		<nav class="flex justify-around">
+			<Link to="/">Home</Link>
+			<Link to="about">About</Link>
+			<Link to="blog">Blog</Link>
+		</nav>
+	
+		<main class="mt-8">
+			<Route path="/">
+				<h1 class="outline-none text-7xl text-center ring-pink-600 text-red-700">Home</h1>
+			</Route>
+	
+			<Route path="about">
+				<h1 class="outline-none text-7xl text-center text-red-700">About</h1>
+			</Route>
+			
+			<Route path="blog/*">
+				<Route path="/">
+					<h1 class="outline-none text-7xl text-center text-red-700">Blog</h1>
+					<Link to="1">Blog 1</Link>
+					<Link to="2">Blog 2</Link>
+				</Route>
+				<Route path=":id" let:params>
+					<h1 class="outline-none text-7xl text-center text-red-700">Blog {params.id}</h1>
+				</Route>
+			</Route>
+		</main>
+	</div>
+</Router>

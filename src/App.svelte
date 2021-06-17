b5e204f4e2d4ef30643d34f39b8c2241190dd972
Alvaro R. S. Dziadzio <dziadzio.alvaro@gmail.com>
2021-06-17 17:35:57 -0300

    🔖 Homepage template finished

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,32 @@
-<div class="bg-gradient-to-b lg:bg-gradient-to-r from-white to-gray-200">
-	<main class="container mx-auto p-8 md:px-20 lg:p-8 font-work-sans min-h-screen lg:flex">
-		
-		<div class="lg:sticky lg:top-8 lg:w-2/5 xl:w-1/3 self-start">
+<div class="font-work-sans bg-gradient-to-b lg:bg-gradient-to-r from-white to-gray-200">
+	<div class="container mx-auto p-8 md:px-20 lg:p-8 flex flex-col min-h-screen">
+
+		<div class="lg:flex flex-grow">
+			<div class="lg:w-2/5 xl:w-1/3">
+				<div class="lg:sticky lg:top-8 mb-12">
 			
-			<header class="mb-14 md:w-3/5 lg:w-auto">
-				<h1 class="outline-none py-2 text-4xl lg:text-6xl mb-3 lg:mb-6 font-semibold bg-gradient-to-br from-indigo-400 to-purple-800 bg-clip-text text-transparent">Welcome to my realm of personal projects</h1>
-				<h2 class="text-xl font-light lg:text-3xl text-gray-600">Also my portfolio</h2>
-			</header>
+					<!-- Header -->
+					<header class="mb-14 sm:w-3/5 lg:w-auto">
+						<h1 class="outline-none py-2 text-4xl lg:text-6xl mb-3 lg:mb-6 font-semibold bg-gradient-to-br from-indigo-400 to-purple-800 bg-clip-text text-transparent">Welcome to my realm of personal projects</h1>
+						<h2 class="text-xl font-light lg:text-3xl text-gray-600">Also my portfolio</h2>
+					</header>
 			
-			<!-- About -->
-			<div class="mb-12 lg:w-auto">
-				<h3 class="font-bold mb-2">About Me</h3>
-				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
+					<!-- About -->
+					<section class="">
+						<h3 class="font-bold mb-2">About Me</h3>
+						<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
+					</section>
+				</div>
+			</div>
+			
+			<div class="lg:w-3/5 xl:w-2/3 my-16 lg:px-10 flex flex-col justify-center">
+				<p class="text-2xl sm:text-3xl md:text-4xl text-center font-light text-gray-400 transform lg:rotate-12">Nothing to see here... <br>Yet</p>
 			</div>
 		</div>
+	
+		<footer>
+			<p class="text-xs sm:text-sm text-gray-600">Copyright © 2021 <strong>Álvaro Dziadzio</strong>. All Rights Reserved.</p>
+		</footer>
 		
-		<!-- <img class="hidden md:block lg:hidden w-1/2 align-middle absolute top-0 right-0" src="img/flandre.png" alt="flandre scarlet"> -->
-		
-		<div class="lg:px-8 lg:w-3/5 xl:w-2/3">
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti et veritatis quos. Quas sequi doloremque fugit labore magni similique autem aut, explicabo error odio esse repellendus repudiandae perferendis illum quo.</p>
-		</div>
-		
-	</main>
+	</div>
 </div>
